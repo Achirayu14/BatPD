80bc73f822eb252d0e553331f00a6905c0565d2e
Achirayu14 <achikp.9800@gmail.com>
2026-05-16 17:53:56 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,18 @@
 <?php
-$host     = 'sql12.freesqldatabase.com';
-$db_name  = 'sql12826794';
-$username = 'sql12826794';
-$password = 'zwrjS4a8DvPort';
-$port     = '3306';
+$host     = 'mysql-224eff1f-achikp-5496.c.aivencloud.com';
+$db_name  = 'defaultdb';
+$username = 'avnadmin';
+$password = 'AVNS_f9Lm0JvFIN0ETYo-x8i';
+$port     = '27178';
 
 try {
     $conn = new PDO(
         "mysql:host=$host;port=$port;dbname=$db_name;charset=utf8mb4",
-        $username, $password
+        $username, $password,
+        [
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            PDO::MYSQL_ATTR_SSL_CA => true,
+        ]
     );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
